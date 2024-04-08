cf6ca8b2402e821a13e0c06bdf8f93a8cda18c6a
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-04-08 16:52:19 +0800

    fov extraction

--- dataloaders/utils.py
+++ dataloaders/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import time
 import hashlib
+import cv2
 
 def get_unk_mask_indices(image,testing,num_labels,known_labels):
     if testing:
@@ -21,6 +22,17 @@ def get_unk_mask_indices(image,testing,num_labels,known_labels):
 
     return unk_mask_indices
 
+def fov_extractor(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 30, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    c = max(contours, key=cv2.contourArea)
+    mask = np.zeros_like(gray)
+    cv2.drawContours(mask, [c], -1, 255, thickness=cv2.FILLED)
+    result = cv2.bitwise_and(image, image, mask=mask)
+    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+    return Image.fromarray(result)
+
 def image_loader(path,transform):
     try:
         image = Image.open(path)
@@ -30,7 +42,8 @@ def image_loader(path,transform):
         image = Image.open(path)
 
     image = image.convert('RGB')
-
+    # image = fov_extractor(np.array(image))
+    
     if transform is not None:
         image = transform(image)
 
