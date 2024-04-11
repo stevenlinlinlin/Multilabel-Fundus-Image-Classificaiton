b9a110eb71da0da993907a5fa42fa59567bd0650
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-04-11 13:05:57 +0800

    resnet152

--- models/resnet.py
+++ models/resnet.py
@@ -18,3 +18,21 @@ class ResNet50(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+class ResNet152(nn.Module):
+    def __init__(self, num_classes):
+        super(ResNet152, self).__init__()
+        resnet152 = models.resnet152(weights='DEFAULT')
+        num_features = resnet152.fc.in_features # 2048
+        resnet152.fc = nn.Sequential(
+            # nn.BatchNorm1d(num_features),
+            nn.Linear(num_features, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            # nn.Dropout(0.5),
+            nn.Linear(512, num_classes),
+        )
+        self.model = resnet152
+
+    def forward(self, x):
+        return self.model(x)
