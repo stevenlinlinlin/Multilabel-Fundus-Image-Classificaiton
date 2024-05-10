0bc36243fc0647b40537b370b4fb2e24c456d41f
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-05-10 11:22:55 +0800

    convnext model

---
+++ models/convnext.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torchvision.models as models
+from models.utils import weights_init
+
+class ConvNeXt(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.convnext = models.convnext_large(weights='DEFAULT')
+        self.convnext.classifier = nn.Sequential(
+            nn.LayerNorm([1536,1,1], elementwise_affine=True),
+            nn.Flatten(start_dim=1,end_dim=-1),
+            nn.Linear(1536, num_classes)
+        )
+        self.convnext.classifier.apply(weights_init)
+
+    def forward(self, x):
+        return self.convnext(x)
