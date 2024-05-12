e06c9bbb0a65913f61c8aa171d40281caec2d333
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-05-13 01:01:30 +0800

    use timm

--- models/convnext.py
+++ models/convnext.py
@@ -7,13 +7,13 @@ class ConvNeXt(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
          # self.convnext = models.convnext_large(weights='DEFAULT')
-        self.convnext = timm.create_model('convnextv2_large', pretrained=True)
-        self.convnext.classifier = nn.Sequential(
-            nn.LayerNorm([1536,1,1], elementwise_affine=True),
-            nn.Flatten(start_dim=1,end_dim=-1),
-            nn.Linear(1536, num_classes)
-        )
-        self.convnext.classifier.apply(weights_init)
+        self.convnext = timm.create_model('convnextv2_large.fcmae_ft_in22k_in1k_384', pretrained=True, num_classes=num_classes)
+        # self.convnext.classifier = nn.Sequential(
+        #     nn.LayerNorm([1536,1,1], elementwise_affine=True),
+        #     nn.Flatten(start_dim=1,end_dim=-1),
+        #     nn.Linear(1536, num_classes)
+        # )
+        # self.convnext.classifier.apply(weights_init)
 
     def forward(self, x):
         return self.convnext(x)
--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -6,10 +6,10 @@ from models.utils import weights_init
 class SwinTransformer(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        self.swin = models.swin_v2_b(weights='DEFAULT')
-        # self.swin = timm.create_model('swinv2_large_window12to24_192to384', pretrained=True)
-        self.swin.head = nn.Linear(self.swin.head.in_features, num_classes)
-        self.swin.head.apply(weights_init)
+        # self.swin = models.swin_v2_b(weights='DEFAULT')
+        self.swin = timm.create_model('swinv2_base_window12to24_192to384.ms_in22k_ft_in1k', pretrained=True, num_classes=num_classes)
+        # self.swin.head = nn.Linear(self.swin.head.in_features, num_classes)
+        # self.swin.head.apply(weights_init)
 
     def forward(self, x):
         return self.swin(x)
