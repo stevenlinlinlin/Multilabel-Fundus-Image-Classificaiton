9e2e669df73a06bf68a6450b60f1eddb16cbe11a
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-05-16 19:07:28 +0800

    add GAP

--- models/myconvnext.py
+++ models/myconvnext.py
@@ -10,6 +10,7 @@ class ConvNeXtTransformer(nn.Module):
         super(ConvNeXtTransformer, self).__init__()
         self.features = timm.create_model('convnextv2_large.fcmae_ft_in22k_in1k_384', pretrained=True, num_classes=0)
         self.flatten = nn.Flatten(start_dim=2, end_dim=-1)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
         d_model = 1536
         self.layer_norm = nn.LayerNorm(d_model)
         encoder_layers = TransformerEncoderLayer(d_model, nhead, dim_feedforward, batch_first=True)
@@ -18,12 +19,14 @@ class ConvNeXtTransformer(nn.Module):
         
     def forward(self, x):
         x = self.features.forward_features(x)
+        # x_gap = self.gap(x)
         x = self.flatten(x)
         x = x.permute(0, 2, 1)
         x = self.layer_norm(x)
         x = self.transformer_encoder(x)
         x = x.permute(0, 2, 1)
         x = torch.mean(x,dim=2)
+        # x = torch.cat((x_gap.view(x_gap.size(0), -1), x), dim=1)
         x = self.head(x)
         return x
     
