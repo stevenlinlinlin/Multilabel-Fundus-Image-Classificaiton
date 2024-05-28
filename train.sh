16e90a2411ebe33b602947db38712f21adbf19e2
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-05-29 00:41:44 +0800

    name

--- train.sh
+++ train.sh
@@ -20,36 +20,37 @@
 
 # CUDA_VISIBLE_DEVICES=$1 python train.py \
 #     --model "convnext" \
-#     --save_results_path "results/convnextV2-L_in22k_384_1.csv" \
+#     --save_results_path "results/convnextV2-L.csv" \
 #     --val \
 #     --normal_class 0 \
 #     --dataset "rfmid"
 
 # CUDA_VISIBLE_DEVICES=$1 python train.py \
 #     --model "swin" \
-#     --save_results_path "results/swinV2-B_in22k_384_1.csv" \
+#     --save_results_path "results/swinV2-B.csv" \
 #     --val \
 #     --normal_class 0 \
 #     --dataset "rfmid"
 
 # CUDA_VISIBLE_DEVICES=$1 python train.py \
 #     --model "efficientnet" \
-#     --save_results_path "results/efficientnetB7_384_dropout_1.csv" \
+#     --save_results_path "results/efficientnetB7.csv" \
 #     --val \
 #     --normal_class 0 \
 #     --dataset "rfmid"
 
 CUDA_VISIBLE_DEVICES=$1 python train.py \
     --model "densenet" \
-    --save_results_path "results/test.csv" \
+    --save_results_path "results/densenet161.csv" \
     --val \
     --normal_class 0 \
     --dataset "rfmid"
 
 # CUDA_VISIBLE_DEVICES=$1 python train.py \
 #     --model "ctran" \
-#     --save_results_path "results/ctran_384.csv" \
+#     --save_results_path "results/ctran.csv" \
 #     --ctran_model \
 #     --val \
 #     --normal_class 0 \
-#     --dataset "rfmid"
+#     --dataset "rfmid" \
+#     --lr 0.00001
