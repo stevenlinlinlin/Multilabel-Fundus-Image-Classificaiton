1a35ec2fb94afb5cd9157c96d70b36897fc44b30
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-06-13 12:10:54 +0800

    add vit-l

--- models/vit.py
+++ models/vit.py
@@ -1,5 +1,6 @@
 import torchvision.models as models
 import torch.nn as nn
+import timm
 from transformers import ViTForImageClassification, AutoImageProcessor
 
 
@@ -16,4 +17,13 @@ class ViTForMultiLabelClassification(nn.Module):
         inputs = self.processor(images=image, return_tensors="pt")
         outputs = self.vit(inputs)
         return outputs.logits
+    
+
+class ViT(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.vit_l = timm.create_model('vit_large_patch16_384.augreg_in21k_ft_in1k', pretrained=True, num_classes=num_classes)
+
+    def forward(self, x):
+        return self.vit_l(x)
    
