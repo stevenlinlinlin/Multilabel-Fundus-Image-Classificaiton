e8ed775bd855cc3212bfcc4854133d88f17679c5
tcl <steven11922146@cmlab.csie.ntu.edu.tw>
2024-06-27 10:39:12 +0800

    vit

--- train.py
+++ train.py
@@ -157,7 +157,7 @@ def get_model(model_name, transformer_layer, num_classes):
     elif model_name == 'inception':
         model = InceptionV3(num_classes).to(device)
     elif model_name == 'vit':
-        model = ViT(num_labels=num_classes).to(device)
+        model = ViT(num_classes).to(device)
     elif model_name == 'ctran':
         model = CTranModel(num_labels=num_classes,use_lmt=True,pos_emb=False,layers=3,heads=4,dropout=0.1).to(device)
     elif model_name == 'swin':
